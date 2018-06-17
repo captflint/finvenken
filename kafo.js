ea41e71b041422a85d7c09ac78d300d965c5db3b
James Stephenson <c4p7.fl1n7@gmail.com>
2018-06-17 09:34:58 -0400

    pli da lernado (pri setInterval).

--- kafo.js
+++ kafo.js
@@ -1,13 +1,24 @@
 var esperantistoj = 0;
 var esperantistojEle = document.getElementById("esperantistoj");
 var mono = 0;
+var monoEle = document.getElementById("mono");
 
 function varbi(n) {
 	esperantistoj += n;
 	esperantistojEle.innerHTML = esperantistoj;
 }
 
+function ĝisdatigiNombrojn() {
+	mono += esperantistoj;
+	let spesmiloj = mono / 1000;
+	esperantistojEle.innerHTML = esperantistoj;
+	monoEle.innerHTML = spesmiloj;
+}
+
+var ĝisdatigiSekunde = setInterval(ĝisdatigiNombrojn, 1000);
+
 var flagoEle = document.getElementById("flago");
 flagoEle.setAttribute("onclick", "varbi(1);");
 
 esperantistojEle.innerHTML = esperantistoj;
+monoEle.innerHTML = mono;
