8b7cd5682650862e22b8e8f802d8638e37c19822
James Stephenson <c4p7.fl1n7@gmail.com>
2018-06-17 12:08:00 -0400

    La nombroj nun estas pli legeblaj.

--- kafo.js
+++ kafo.js
@@ -5,14 +5,72 @@ var monoEle = document.getElementById("mono");
 
 function varbi(n) {
 	esperantistoj += n;
-	esperantistojEle.innerHTML = esperantistoj;
+	esperantistojEle.innerHTML = komigiNombron(esperantistoj);
+}
+
+function komigiNombron(n) {
+	n = "" + n;
+	let s = "";
+	let x = 3;
+	let l = n.length;
+	while (l > 0) {
+		l -= 1;
+		if (x > 0) {
+			s = n[l] + s;
+			x -= 1;
+		}
+		else {
+			x = 2;
+			s = n[l] + "," + s;
+		}
+	}
+	return(s);
+}
+
+function spesojAlSpesmiloj(m) {
+	m = "" + m;
+	let s = "";
+	let x = 3;
+	let l = m.length;
+	let punkto = true;
+	if (m.length == 0) {
+		return("nul");
+	}
+	else if (m.length == 1) {
+		return("0.00" + m);
+	}
+	else if (m.length == 2) {
+		return("0.0" + m);
+	}
+	else if (m.length == 3) {
+		return("0." + m);
+	}
+	else {
+		while (l > 0) {
+			l -= 1;
+			if (x > 0) {
+				s = m[l] + s;
+				x -= 1;
+			}
+			else {
+				x = 2;
+				if (punkto) {
+					s = m[l] + "." + s;
+					punkto = false;
+				}
+				else {
+					s = m[l] + "," + s;
+				}
+			}
+		}
+		return(s);
+	}
 }
 
 function ĝisdatigiNombrojn() {
 	mono += esperantistoj;
-	let spesmiloj = mono / 1000;
-	esperantistojEle.innerHTML = esperantistoj;
-	monoEle.innerHTML = spesmiloj;
+	esperantistojEle.innerHTML = komigiNombron(esperantistoj);
+	monoEle.innerHTML = spesojAlSpesmiloj(mono);
 }
 
 var ĝisdatigiSekunde = setInterval(ĝisdatigiNombrojn, 1000);
@@ -20,5 +78,5 @@ var ĝisdatigiSekunde = setInterval(ĝisdatigiNombrojn, 1000);
 var flagoEle = document.getElementById("flago");
 flagoEle.setAttribute("onclick", "varbi(1);");
 
-esperantistojEle.innerHTML = esperantistoj;
-monoEle.innerHTML = mono;
+esperantistojEle.innerHTML = komigiNombron(esperantistoj);
+monoEle.innerHTML = spesojAlSpesmiloj(mono);
